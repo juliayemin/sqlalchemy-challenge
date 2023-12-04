14a79f37877e8b3459995cef9ee0c54742286814
Thetsu Ye Min <juliayemints@gmail.com>
2023-12-03 18:59:41 -0800

    upload

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,71 @@
-# Import the dependencies.
+from flask import Flask, jsonify
+from sqlalchemy import create_engine, func
+from sqlalchemy.orm import Session
+from sqlalchemy.ext.automap import automap_base
+import numpy as np
+import datetime as dt
 
+# Database setup
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
-
-#################################################
-# Database Setup
-#################################################
-
-
-# reflect an existing database into a new model
-
-# reflect the tables
-
-
-# Save references to each table
-
-
-# Create our session (link) from Python to the DB
-
-
-#################################################
 # Flask Setup
-#################################################
+app = Flask(__name__)
 
+# Home Route
+@app.route("/")
+def welcome():
+    """List all available API routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
+    )
+#Parecipitation route
+@app.route("/")
+def welcome():
+    """List all available API routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
+    )
 
+#Stations Route
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+    results = session.query(Station.station).all()
+    session.close()
 
+    stations = list(np.ravel(results))
+    return jsonify(stations)
 
-#################################################
-# Flask Routes
-#################################################
+#TOBS route
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
+    latest_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    latest_date = latest_date[0]
+    one_year_ago = dt.datetime.strptime(latest_date, "%Y-%m-%d") - dt.timedelta(days=365)
+
+    results = session.query(Measurement.tobs).\
+        filter(Measurement.station == 'most_active_station').\
+        filter(Measurement.date >= one_year_ago).all()
+    session.close()
+
+    temperatures = list(np.ravel(results))
+    return jsonify(temperatures)
+
+# Flask App Run
+if __name__ == '__main__':
+    app.run(debug=True)
